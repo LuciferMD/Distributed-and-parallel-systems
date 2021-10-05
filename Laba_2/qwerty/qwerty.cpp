0e6a2403e60868a12161da7fd14440c26828efe6
LuciferMD <78922242+LuciferMD@users.noreply.github.com>
2021-10-05 16:19:31 +0300

    Add files via upload

---
+++ Laba_2/qwerty/qwerty.cpp
@@ -0,0 +1,144 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <ctime>
+#include <thread>
+
+using namespace std;
+int K, f, n, m;
+double a, b, c, d, dx, dy;
+double Result = 0;
+
+typedef struct MyData {
+	double xi;
+	double yj;
+} MYDATA, * PMYDATA;
+
+vector<MYDATA> cells;
+vector<double> results;
+
+void console_data_input();
+double function_one(double, double);
+double function_two(double, double);
+double function_three(double, double);
+double function_four(double, double);
+double func(double, double);
+
+bool rectangular_integral();
+DWORD WINAPI calculate(LPVOID);
+
+
+bool rectangular_integral() {
+	dx = (b - a) / n; //Count the legth of each element
+	dy = (d - c) / m;
+	double I = 0;
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			MYDATA data;
+			data.xi = a + dx / 2 + i * dx;			// Method of medium rectangles
+			data.yj = c + dy / 2 + j * dy;
+			cells.push_back(data);
+		}
+	}
+
+	HANDLE* threads = new HANDLE[K];
+	for (int k = 0; k < K; k++) {
+		int p = k;
+		threads[k] = CreateThread(NULL, 0, calculate, (LPVOID)p, NULL, NULL);
+	}
+
+	WaitForMultipleObjects(K, threads, TRUE, INFINITE);
+
+	return true;
+}
+
+DWORD WINAPI calculate(LPVOID lpParam) {
+	double I = 0;
+	for (unsigned int i = (int)lpParam; i < cells.size(); i += K) {
+		MYDATA data = cells[i];
+		I += dx * dy * func(data.xi, data.yj);
+	}
+
+	results.push_back(I);
+	return (DWORD)1;
+}
+
+
+void console_data_input() {
+	cout << "Calculating the double integral of a function f(x) by area D=[a,b]x[c,d]" << endl << endl;
+	cout << "Count Threads (K) : ";
+	cin >> K;
+	cout << "Area D" << endl;
+	cout << "(a) : ";
+	cin >> a;
+	cout << "(b) : ";
+	cin >> b;
+	cout << "(c) : ";
+	cin >> c;
+	cout << "(d) : ";
+	cin >> d;
+	cout << "How many divided parts [a,b] (n) : ";
+	cin >> n;
+	cout << "How many divided parts [c,d] (m) : ";
+	cin >> m;
+	cout << "\tChoose functions : " << endl;
+	cout << "1. e^(x) * sin(y)" << endl;
+	cout << "2. sin(x + 2 * y)" << endl;
+	cout << "3. cos(x * e^(y)" << endl;
+	cout << "4. exp(cos(x^(2) + y )" << endl;
+	cin >> f;
+}
+
+
+double func(double x, double y) {
+	switch (f) {
+	case 1:
+		return function_one(x, y);
+	case 2:
+		return function_two(x, y);
+	case 3:
+		return function_three(x, y);
+	case 4:
+		return function_four(x, y);
+	default:
+		return 0;
+	}
+}
+
+double function_one(double x, double y) {
+	return exp(x) * sin(y);
+};
+
+double function_two(double x, double y) {
+	return sin(x + 2 * y);
+};
+
+double function_three(double x, double y) {
+	return cos(x * exp(y));
+}		
+
+double function_four(double x, double y) {
+	return exp(cos(pow(x, 2) + y));
+}
+	
+
+int main() {
+	
+	console_data_input();
+
+	auto begin = chrono::steady_clock::now();
+
+	rectangular_integral();
+	for (int i = 0; i < results.size(); i++)
+		Result += results[i];
+
+	auto end = chrono::steady_clock::now();
+	auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+
+	cout << "Result: " << Result << std::endl;
+	cout << "Time: " << elapsed_ms.count() << " ms\n";
+	system("pause");
+
+	return 0;
+}
---
+++ Laba_2/qwerty3/qwerty3.cpp
@@ -0,0 +1,133 @@
+﻿#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <math.h>
+#include <thread>
+#include <Windows.h>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+
+int K, n;
+int** matrix_1;
+int** matrix_2;
+int** matrix_mul_simple;
+int** matrix_mul_multip;
+long run_time = 0;
+
+std::ofstream simple_mul("..//OneThread.txt");
+std::ofstream multip_mul("..//ManyThreaads.txt");
+
+HANDLE* threads;
+
+
+
+void generate_matrix() {
+	matrix_1 = new int* [n];
+	matrix_2 = new int* [n];
+	matrix_mul_simple = new int* [n];
+	matrix_mul_multip = new int* [n];
+
+	for (int i = 0; i < n; i++) {
+		matrix_1[i] = new int[n];
+		matrix_2[i] = new int[n];
+		matrix_mul_simple[i] = new int[n];
+		matrix_mul_multip[i] = new int[n];
+	}
+
+	srand(time(0));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			matrix_1[i][j] = 1 + rand() % 100;
+			matrix_2[i][j] = 1 + rand() % 100;
+			matrix_mul_simple[i][j] = 0;
+			matrix_mul_multip[i][j] = 0;
+		};
+	};
+}
+
+void print_matrix(int** matrix, int border, std::ostream& stream) {
+	stream << "Time: " << run_time << " ms." << std::endl;
+	stream.setf(std::ios::right);
+	stream << std::endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			stream << std::setw(border) << matrix[i][j] << " ";
+		}
+		stream << std::endl;
+	}
+	stream << std::endl;
+}
+
+
+void input_data() {
+	std::cout << "Calculating the product of two matrices of order n" << std::endl;
+	std::cout << "Enter n: ";
+	std::cin >> n;
+	std::cout << "Enter К: ";
+	std::cin >> K;
+	if (K > 64)
+		K = 64;
+	threads = new HANDLE[K];
+}
+
+DWORD WINAPI calculate(LPVOID lpParam) {
+	int num = (int)lpParam;
+	int row;	
+	int col;
+	for (int i = num; i < n * n; i += K) {
+		row = i / n;
+		col = i % n;
+		for (int inner = 0; inner < n; inner++) {
+			matrix_mul_multip[row][col] += matrix_1[row][inner] * matrix_2[inner][col];
+		}
+	}
+	return NULL;
+}
+
+void multiply_simple(int** matrix1, int** matrix2, int**& result) {
+	for (int row = 0; row < n; row++) {
+		for (int col = 0; col < n; col++) {
+			for (int inner = 0; inner < n; inner++) {
+				result[row][col] += matrix1[row][inner] * matrix2[inner][col];
+			}
+		}
+	}
+}
+
+void multiply_multip() {
+	int counter = 0;
+
+	for (int j = 0; j < K; j++) {
+		int p = j;
+		threads[j] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)calculate, (LPVOID)p, NULL, NULL);
+	}
+
+	WaitForMultipleObjects(K, threads, TRUE, INFINITE);
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	input_data();
+	generate_matrix();
+
+	auto begin_1 = std::chrono::steady_clock::now();
+	multiply_simple(matrix_1, matrix_2, matrix_mul_simple);
+	auto end_1 = std::chrono::steady_clock::now();
+	auto elapsed_ms_1 = std::chrono::duration_cast<std::chrono::milliseconds>(end_1 - begin_1);
+	run_time = elapsed_ms_1.count();
+
+	print_matrix(matrix_mul_simple, 6, simple_mul);
+
+	auto begin_2 = std::chrono::steady_clock::now();
+	multiply_multip();
+	auto end_2 = std::chrono::steady_clock::now();
+	auto elapsed_ms_2 = std::chrono::duration_cast<std::chrono::milliseconds>(end_2 - begin_2);
+	run_time = elapsed_ms_2.count();
+
+	print_matrix(matrix_mul_multip, 6, multip_mul);
+
+	system("pause");
+	return 0;
+}
