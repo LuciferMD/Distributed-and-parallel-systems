8bf00d380bc3353665a097f8d04b5dba5f9ce491
LuciferMD <78922242+LuciferMD@users.noreply.github.com>
2021-11-09 22:35:04 +0300

    Add files via upload

---
+++ Laba_4/qwerty/qwerty.cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <ctime>
+#include <thread>
+#include <future>
+#include<omp.h>
+
+using namespace std;
+int K, f, n, m;
+double a, b, c, d, dx, dy;
+double Result = 0;
+
+typedef struct MyData {
+	double xi;
+	double yj;
+} MYDATA, * PMYDATA;
+
+vector<MYDATA> cells;
+vector<double> results;
+
+void console_data_input();
+double function_one(double, double);
+double function_two(double, double);
+double function_three(double, double);
+double function_four(double, double);
+double function(double, double);
+double func(double x, double y);
+double mulmatr(double, double);
+
+bool rectangular_integral();
+int calculate(int);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	console_data_input();
+
+	auto begin = chrono::steady_clock::now();
+
+	rectangular_integral();
+	for (int i = 0; i < results.size(); i++)
+		Result += results[i];
+
+	auto end = chrono::steady_clock::now();
+	auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+
+	cout << "Полученный результат: " << Result << std::endl;
+	cout << "Затраты по времени: " << elapsed_ms.count() << " мс\n";
+	system("pause");
+
+	return 0;
+}
+
+
+bool rectangular_integral() {
+	dx = (b - a) / n;
+	dy = (d - c) / m;
+	double I = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			MYDATA data;
+			data.xi = a + dx / 2 + i * dx;
+			data.yj = c + dy / 2 + j * dy;
+			cells.push_back(data);
+		}
+	}
+	double result = 0;
+	int* threads = new int[K];
+	
+	calculate(0);
+
+
+	return true;
+}
+
+int calculate(int lpParam) {
+	double I = 0;
+	lpParam = K;
+#pragma omp parallel num_threads(K) reduction(+: I) 
+	for (int i = (int)lpParam; i < cells.size(); i += K) {
+		MYDATA data = cells[i];
+		I += dx * dy * func(data.xi, data.yj);
+	}
+
+	results.push_back(I);
+	return 1;
+}
+
+
+void console_data_input() {
+	cout << "двойной интеграл функции f(x) по области D=[a,b]x[c,d]\n\n";
+	cout << "Кол-во потоков (K) : ";
+	cin >> K;
+	cout << "Область D\n";
+	cout << "(a) : ";
+	cin >> a;
+	cout << "(b) : ";
+	cin >> b;
+	cout << "(c) : ";
+	cin >> c;
+	cout << "(d) : ";
+	cin >> d;
+	cout << "Частей разбиения [a,b] (n) : ";
+	cin >> n;
+	cout << "Частей разбиения [c,d] (m) : ";
+	cin >> m;
+	cout << "Выберите функцию : \n";
+	cout << "1. e^(x) * sin(y)\n";
+	cout << "2. sin(x + 2 * y)\n";
+	cout << "3. cos(x * e^(y)\n";
+	cout << "4. exp(cos(x^(2) + y )\n";
+	cin >> f;
+}
+
+double func(double x, double y) {
+	switch (f)
+	{
+	case 1:
+		return function_one(x, y);
+	case 2:
+		return function_two(x, y);
+	case 3:
+		return function_three(x, y);
+	case 4:
+		return function_four(x, y);
+	default:
+		return 0;
+	}
+}
+
+
+double function_one(double x, double y) {
+	return exp(x) * sin(y);
+};
+
+double function_two(double x, double y) {
+	return sin(x + 2 * y);
+};
+
+double function_three(double x, double y) {
+	return cos(x * exp(y));
+}
+
+double function_four(double x, double y) {
+	return exp(cos(pow(x, 2) + y));
+}
+double mulmatr(double, double) {
+	return 0;
+}
---
+++ Laba_4/qwerty3/qwerty3.cpp
@@ -0,0 +1,151 @@
+﻿// lab2.cpp: определяет точку входа для консольного приложения.
+//
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <math.h>
+#include <thread>
+#include <Windows.h>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include<future>
+#include<omp.h>
+
+int K, n;
+int** matrix_1;
+int** matrix_2;
+int** matrix_mul_simple;
+int** matrix_mul_multip;
+long run_time = 0;
+
+typedef struct MyData {
+	int i;
+	int j;
+} MYDATA, * PMYDATA;
+
+void input_data();
+void generate_matrix();
+void print_matrix(int**, int, std::ostream&);
+void multiply_simple(int**, int**, int**&);
+void multiply_multip();
+int calculate(int);
+
+std::ofstream single_mul("..//single_mul.txt");
+std::ofstream multi_mul("..//multi_mul.txt");
+
+HANDLE* threads;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	input_data();
+	generate_matrix();
+
+	std::cout << "ожидайте результат..." << std::endl;
+
+	auto begin_1 = std::chrono::steady_clock::now();
+	multiply_simple(matrix_1, matrix_2, matrix_mul_simple);
+	auto end_1 = std::chrono::steady_clock::now();
+	auto elapsed_ms_1 = std::chrono::duration_cast<std::chrono::milliseconds>(end_1 - begin_1);
+	run_time = elapsed_ms_1.count();
+
+	print_matrix(matrix_mul_simple, 6, single_mul);
+
+	auto begin_2 = std::chrono::steady_clock::now();
+	multiply_multip();
+	auto end_2 = std::chrono::steady_clock::now();
+	auto elapsed_ms_2 = std::chrono::duration_cast<std::chrono::milliseconds>(end_2 - begin_2);
+	run_time = elapsed_ms_2.count();
+
+	print_matrix(matrix_mul_multip, 6, multi_mul);
+
+	printf("Завершено.\n");
+	system("pause");
+	return 0;
+}
+
+void generate_matrix() {
+	matrix_1 = new int* [n];
+	matrix_2 = new int* [n];
+	matrix_mul_simple = new int* [n];
+	matrix_mul_multip = new int* [n];
+
+	for (int i = 0; i < n; i++) {
+		matrix_1[i] = new int[n];
+		matrix_2[i] = new int[n];
+		matrix_mul_simple[i] = new int[n];
+		matrix_mul_multip[i] = new int[n];
+	}
+
+	srand(time(0));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			matrix_1[i][j] = 1 + rand() % 100;
+			matrix_2[i][j] = 1 + rand() % 100;
+			matrix_mul_simple[i][j] = 0;
+			matrix_mul_multip[i][j] = 0;
+		};
+	};
+}
+
+void print_matrix(int** matrix, int border, std::ostream& stream) {
+	stream << "Время: " << run_time << " ms." << std::endl;
+	stream.setf(std::ios::right);
+	stream << std::endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			stream << std::setw(border) << matrix[i][j] << " ";
+		}
+		stream << std::endl;
+	}
+	stream << std::endl;
+}
+
+
+void input_data() {
+	std::cout << "Приложение для вычисления произведения двух матриц порядка n\n\n";
+	std::cout << "Введите n: ";
+	std::cin >> n;
+	std::cout << "Введите К: ";
+	std::cin >> K;
+	if (K > 64)
+		K = 64;
+	threads = new HANDLE[K];
+}
+
+void multiply_multip() {
+	int counter = 0;
+
+	calculate(0);
+
+
+}
+
+int calculate(int lpParam) {
+	
+	int row;
+	int col;
+	int inner;
+	#pragma omp parallel for private(row,inner,col) shared(matrix_mul_multip, matrix_1,matrix_2) num_threads(K)
+	for (int i = 0; i < n * n; i++) {
+		row = i / n;
+		col = i % n;
+		for (inner = 0; inner < n; inner++) {
+			matrix_mul_multip[row][col] += matrix_1[row][inner] * matrix_2[inner][col];
+		}
+	}
+	return NULL;
+}
+
+void multiply_simple(int** matrix1, int** matrix2, int**& result) {
+	for (int row = 0; row < n; row++) {
+		for (int col = 0; col < n; col++) {
+			for (int inner = 0; inner < n; inner++) {
+				result[row][col] += matrix1[row][inner] * matrix2[inner][col];
+			}
+		}
+	}
+}
+
